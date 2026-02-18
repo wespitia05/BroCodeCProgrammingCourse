b7fc2dc92b7152df4e149fc76a4012149ee4c3d5
william <espiw@farmingdale.edu>
2026-02-17 20:33:22 -0500

    BroCode c variables setup/notes

---
+++ variables.c
@@ -0,0 +1,9 @@
+// preprocessor directive
+// tells compiler to include standard input/output libraries
+#include <stdio.h>
+
+// function always has to be main
+int main() {
+    // variable = a reusable container for a value
+    //            behaves as if it were the value it contains
+}
