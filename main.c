8e5da692c34b2a90a828907ead7a3f9125c329d8
william <espiw@farmingdale.edu>
2026-02-17 20:31:05 -0500

    BroCode c first lesson

---
+++ main.c
@@ -0,0 +1,8 @@
+// preprocessor directive
+// tells compiler to include standard input/output libraries
+#include <stdio.h>
+
+int main() {
+    printf("i like pizza!\n");
+    printf("it's really good!");
+}
