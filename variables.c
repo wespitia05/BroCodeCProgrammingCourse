6e4ca5599b92df2c99f2c324449f198f0d94870b
william <espiw@farmingdale.edu>
2026-02-17 21:41:32 -0500

    Bro Code c variables printing an integer using a format specififer

--- variables.c
+++ variables.c
@@ -6,4 +6,8 @@
 int main() {
     // variable = a reusable container for a value
     //            behaves as if it were the value it contains
+
+    int age = 25;
+    // % = format specifier, d = decimal
+    printf("you are %d years old", age);
 }
