6ee9e823ff0450cdaf5e5a222138633bfe527312
william <espiw@farmingdale.edu>
2026-02-18 21:52:06 -0500

    few starting examples of format specifiers

--- format_specifiers.c
+++ format_specifiers.c
@@ -8,5 +8,17 @@ int main() {
     //                    and optional modifiers (width, precision, flags)
     //                    they control how data is displayed or interpreted
 
+    int age = 25;
+    float price = 19.99;
+    double pi = 3.1415926535;
+    char currency = '$';
+    char name[] = "william";
+
+    printf("%d", age); // displays decimal
+    printf("%f", price); // displays floating point
+    printf("%lf", pi); // displays long floating point
+    printf("%c", currency); // displays character
+    printf("%s", name); // displays string (arracy of characters)
+
     return 0;
 }
