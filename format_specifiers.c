cdc5d2d43666e5ff6420bae2f05563018e6f81d4
william <espiw@farmingdale.edu>
2026-02-18 19:13:38 -0500

    format specifiers setup

---
+++ format_specifiers.c
@@ -0,0 +1,12 @@
+// preprocessor directive
+// tells compiler to include standard input/output libraries
+#include <stdio.h>
+
+int main() {
+    // format specifier = special tokens that begin with a % symbol,
+    //                    followed by a character that specfifies the data type
+    //                    and optional modifiers (width, precision, flags)
+    //                    they control how data is displayed or interpreted
+
+    return 0;
+}
