2be706b54ceb0e308810fa424e2c0fc287ec41fe
william <espiw@farmingdale.edu>
2026-02-21 12:23:22 -0500

    story to insert the mad libs in

--- mad_libs.c
+++ mad_libs.c
@@ -31,11 +31,10 @@ int main() {
     fgets(adjective3, sizeof(adjective3), stdin);
     adjective3[strlen(adjective3) - 1] = '\0';
 
-    printf("%s\n", noun);
-    printf("%s\n", verb);
-    printf("%s\n", adjective1);
-    printf("%s\n", adjective2);
-    printf("%s\n", adjective3);
+    printf("\ntoday i went to a %s zoo\n", adjective1);
+    printf("in an exhibit i saw a %s\n", noun);
+    printf("%s was %s and %s!\n", noun, adjective2, verb);
+    printf("i was %s!", adjective3);
 
     return 0;
 }
