9b67ceb8dfc509907379697b1c7abc180d0adc72
william <espiw@farmingdale.edu>
2026-02-21 12:20:11 -0500

    remove the new line space from pressing enter

--- mad_libs.c
+++ mad_libs.c
@@ -13,18 +13,23 @@ int main() {
 
     printf("enter an adjective (description): ");
     fgets(adjective1, sizeof(adjective1), stdin);
+    adjective1[strlen(adjective1) - 1] = '\0';
 
     printf("enter a noun (animal or person): ");
     fgets(noun, sizeof(noun), stdin);
+    noun[strlen(noun) - 1] = '\0';
 
     printf("enter an adjective (description): ");
     fgets(adjective2, sizeof(adjective2), stdin);
+    adjective2[strlen(adjective2) - 1] = '\0';
 
     printf("enter a verb (ending with -ing): ");
     fgets(verb, sizeof(verb), stdin);
+    verb[strlen(verb) - 1] = '\0';
 
     printf("enter an adjective (description): ");
     fgets(adjective3, sizeof(adjective3), stdin);
+    adjective3[strlen(adjective3) - 1] = '\0';
 
     printf("%s\n", noun);
     printf("%s\n", verb);
