30b53c780b7331dfe35e50486e6ef427e37670ff
william <espiw@farmingdale.edu>
2026-02-21 12:18:19 -0500

    getting user input and checking to make sure we're receiving it correctly

--- mad_libs.c
+++ mad_libs.c
@@ -12,10 +12,25 @@ int main() {
     char adjective3[50] = "";
 
     printf("enter an adjective (description): ");
+    fgets(adjective1, sizeof(adjective1), stdin);
+
     printf("enter a noun (animal or person): ");
+    fgets(noun, sizeof(noun), stdin);
+
     printf("enter an adjective (description): ");
+    fgets(adjective2, sizeof(adjective2), stdin);
+
     printf("enter a verb (ending with -ing): ");
+    fgets(verb, sizeof(verb), stdin);
+
     printf("enter an adjective (description): ");
+    fgets(adjective3, sizeof(adjective3), stdin);
+
+    printf("%s\n", noun);
+    printf("%s\n", verb);
+    printf("%s\n", adjective1);
+    printf("%s\n", adjective2);
+    printf("%s\n", adjective3);
 
     return 0;
 }
