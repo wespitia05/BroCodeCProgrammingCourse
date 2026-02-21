a8b9ed1d005763b52e9d910b7725483dec56a2e4
william <espiw@farmingdale.edu>
2026-02-21 13:02:06 -0500

    displaying volume of circle

--- circle_calculator.c
+++ circle_calculator.c
@@ -6,6 +6,7 @@ int main() {
     double radius = 0.0;
     double area = 0.0;
     double surfaceArea = 0.0;
+    double volume = 0.0;
     const double PI = 3.14159; // const so we dont change value of pi
 
     printf("enter the radius: ");
@@ -13,9 +14,11 @@ int main() {
 
     area = PI * pow(radius, 2);
     surfaceArea = 4 * PI * pow(radius, 2);
+    volume = (4.0 / 3.0) * PI * pow(radius, 3);
 
     printf("area: %.2lf\n", area);
     printf("surface area: %.2lf\n", surfaceArea);
+    printf("volume: %.2lf\n", volume);
 
     return 0;
 }
