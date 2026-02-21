7c9b3264bf69e56b7a52a335466cb9e2dac487cf
william <espiw@farmingdale.edu>
2026-02-21 12:58:47 -0500

    displaying area of the circle

--- circle_calculator.c
+++ circle_calculator.c
@@ -3,5 +3,16 @@
 
 int main() {
 
+    double radius = 0.0;
+    double area = 0.0;
+    const double PI = 3.14159; // const so we dont change value of pi
+
+    printf("enter the radius: ");
+    scanf("%lf", &radius);
+
+    area = PI * pow(radius, 2);
+
+    printf("area: %.2lf", area);
+
     return 0;
 }
