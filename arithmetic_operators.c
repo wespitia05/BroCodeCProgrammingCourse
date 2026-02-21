e82c27cc69d6fee774231d559c4427f0e62eb56e
william <espiw@farmingdale.edu>
2026-02-20 19:06:30 -0500

    few examples

--- arithmetic_operators.c
+++ arithmetic_operators.c
@@ -3,5 +3,19 @@
 int main() {
     // arithmetic operators = + - * / % ++ --
 
+    int x = 2;
+    int y = 3;
+    int z = 0;
+
+    // z = x + y; // use int
+    // z = x - y; // use int
+    // z = x * y; // use int
+    // z = x / y; // use float
+    // z = x % y; // use int
+    // x++; // use int
+    // x--; // use int
+
+    printf("%d", z);
+
     return 0;
 }
