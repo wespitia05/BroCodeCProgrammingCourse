5becdc5d2125d7226c964f13632e411ee9d1afda
william <espiw@farmingdale.edu>
2026-02-21 13:00:23 -0500

    displaying surface area of the circle

--- circle_calculator.c
+++ circle_calculator.c
@@ -5,14 +5,17 @@ int main() {
 
     double radius = 0.0;
     double area = 0.0;
+    double surfaceArea = 0.0;
     const double PI = 3.14159; // const so we dont change value of pi
 
     printf("enter the radius: ");
     scanf("%lf", &radius);
 
     area = PI * pow(radius, 2);
+    surfaceArea = 4 * PI * pow(radius, 2);
 
-    printf("area: %.2lf", area);
+    printf("area: %.2lf\n", area);
+    printf("surface area: %.2lf\n", surfaceArea);
 
     return 0;
 }
