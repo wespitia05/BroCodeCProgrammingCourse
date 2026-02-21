6aaea856fc585c880646f8dd398de9adf096bbea
william <espiw@farmingdale.edu>
2026-02-21 12:49:49 -0500

    math function examples

--- math_functions.c
+++ math_functions.c
@@ -3,5 +3,25 @@
 
 int main() {
 
+    int x = 2;
+    float y = 3.14;
+    int z = -3;
+
+    // x = sqrt(x); // square root function
+    // x = pow(x, 2); // square root function pow(base, power of)
+    // y = round(y); // round number
+    // y = ceil(y); // round number up only
+    // y = floor(y); // round number down only
+    // x = abs(x); // returns absolute value of variable
+    // x = abs(x); // returns absolute value of variable
+    // y = log(y); // natural logarithm of variable
+    // x = sin(x); // use sine function (in radians)
+    // x = cos(x); // use cosine function (in radians)
+    // x = tan(x); // use tangent function (in radians)
+
+    printf("%d", x);
+    printf("%f", y);
+    printf("%d", x);
+
     return 0;
 }
