8befc4da03bdc24e6b6c3fa4d69eebb25c8780e4
william <espiw@farmingdale.edu>
2026-02-24 18:20:39 -0500

    quick example using if statements

--- if_statements.c
+++ if_statements.c
@@ -4,5 +4,23 @@ int main() {
     // if statement = do some code if a condition is true
     //                if the condition is false, don't do it
 
+    int age = 12;
+
+    if (age >= 65) {
+        printf("you are a senior");
+    }
+    else if (age >= 18) {
+        printf("you are an adult");
+    }
+    else if (age < 0) {
+        printf("you haven't been born yet");
+    }
+    else if (age == 0) {
+        printf("you are a newborn");
+    }
+    else {
+        printf("you are not an adult");
+    }
+
     return 0;
 }
