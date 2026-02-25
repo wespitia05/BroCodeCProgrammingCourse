21a830245fca7c7fdb78ed3a4c70e95c84f2cc55
william <espiw@farmingdale.edu>
2026-02-25 17:52:02 -0500

    functionality

--- temp_converter.c
+++ temp_converter.c
@@ -4,16 +4,33 @@
 
 int main() {
 
-    char choise = '\0';
+    char choice = '\0';
     float fahrenheit = 0.0f;
     float celsius = 0.0f;
 
-    printf("temperatore conversion calculator");
+    printf("temperature conversion calculator\n");
     printf("C. celsius to fahrenheit\n");
-    printf("F. fahrenheit to celsius");
+    printf("F. fahrenheit to celsius\n");
 
     printf("is the temperature in celsius (C) or fahrenheit (F)?: ");
 
+    scanf("%c", &choice);
+
+    if (choice == 'C' || choice == 'c') {
+        printf("enter the temperature in celsius: ");
+        scanf("%f", &celsius);
+        fahrenheit = (celsius * 9 / 5) + 32; // C to F
+        printf("%.1f celsius is equal to %.1f fahrenheit\n", celsius, fahrenheit);
+    }   
+    else if (choice == 'F' || choice == 'f') {
+        printf("enter the temperature in fahrenheit: ");
+        scanf("%f", &fahrenheit);
+        celsius = (fahrenheit - 32) * 5 / 9; // F to C
+        printf("%.1f fahrenheit is equal to %.1f celsius\n", fahrenheit, celsius);
+    }
+    else {
+        printf("invalid choice. please select C or F\n");
+    }
 
     return 0;
 }
