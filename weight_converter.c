f1ce70d79f064cd81d40bd8b6b17296849d9302b
william <espiw@farmingdale.edu>
2026-02-24 20:10:12 -0500

    declare our variables and prompts

--- weight_converter.c
+++ weight_converter.c
@@ -4,5 +4,15 @@
 
 int main() {
 
+    int choice = 0;
+    float pounds = 0.0f;
+    float kilograms = 0.0f;
+
+    printf("weight conversion calculator\n");
+
+    printf("1. kilograms to pounds\n");
+    printf("2. pounds to kilograms\n");
+    printf("enter your choice (1 or 2): \n");
+
     return 0;
 }
