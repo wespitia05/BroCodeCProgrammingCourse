13bf37a30139363d4e2ff29fa812ec494134468f
william <espiw@farmingdale.edu>
2026-02-24 20:14:42 -0500

    functionality

--- weight_converter.c
+++ weight_converter.c
@@ -19,10 +19,16 @@ int main() {
     if (choice == 1) {
         // kilograms to pounds
         printf("enter the weight in kilograms: ");
+        scanf("%f", &kilograms);
+        pounds = kilograms * 2.20462;
+        printf("%.2f kilograms is equal to %.2f pounds\n", kilograms, pounds);
     }
     else if (choice == 2) {
         // pounds to kilograms
         printf("enter the weight in pounds: ");
+        scanf("%f", &pounds);
+        kilograms = pounds / 2.20462;
+        printf("%.2f pounds is equal to %.2f kilograms\n", pounds, kilograms);
     }
     else {
         printf("invalid choice. please enter 1 or 2.\n");
