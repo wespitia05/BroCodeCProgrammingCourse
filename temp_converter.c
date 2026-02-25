64677ac9ac1aa3e32e991a269563a5b60e8d6df8
william <espiw@farmingdale.edu>
2026-02-25 17:44:43 -0500

    declare our prompts

--- temp_converter.c
+++ temp_converter.c
@@ -8,5 +8,12 @@ int main() {
     float fahrenheit = 0.0f;
     float celsius = 0.0f;
 
+    printf("temperatore conversion calculator");
+    printf("C. celsius to fahrenheit\n");
+    printf("F. fahrenheit to celsius");
+
+    printf("is the temperature in celsius (C) or fahrenheit (F)?: ");
+
+
     return 0;
 }
