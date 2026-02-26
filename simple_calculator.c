8f0ef74196b47ad089f5fcfe9412fbcb85629115
william <espiw@farmingdale.edu>
2026-02-26 17:15:31 -0500

    nested if statement for the division switch

--- simple_calculator.c
+++ simple_calculator.c
@@ -29,8 +29,15 @@ int main() {
             result = num1 * num2;
             break;
         case '/':
-            result = num1 / num2;
+            if(num2 == 0) {
+                printf("you can't divide by zero!");
+            }
+            else {
+                result = num1 / num2;
+            }
             break;
+        default:
+            printf("invalid operator.\n");
     }
 
     printf("result: %.4lf", result);
