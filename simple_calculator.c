ed2e558706fd5d068b6bd516f0520d8e12775c3d
william <espiw@farmingdale.edu>
2026-02-26 17:11:00 -0500

    declare our prompts

--- simple_calculator.c
+++ simple_calculator.c
@@ -9,5 +9,14 @@ int main() {
     double num2 = 0.0;
     double result = 0.0;
 
+    printf("enter the first number: ");
+    scanf("%lf", &num1);
+    
+    printf("enter the operator (+ - * /): ");
+    scanf("%c", &operator);
+
+    printf("enter the second number: ");
+    scanf("%lf", &num2);
+
     return 0;
 }
