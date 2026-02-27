3029394f08fc9bab44154e597d9ec7ec6f668f56
william <espiw@farmingdale.edu>
2026-02-26 23:34:00 -0500

    and & or examples

--- logical_operators.c
+++ logical_operators.c
@@ -7,5 +7,16 @@ int main() {
     // || = OR
     // !  = NOT
 
+    int temp = 35;
+
+    // both conditions MUST be true
+    if(temp > 0 && temp < 30) {
+        printf("the temperature is good");
+    }
+    // only one condition has to be true
+    else if (temp > 30 || temp < 40) {
+        printf("the temperature is bad");
+    }
+
     return 0;
 }
