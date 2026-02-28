fc4d1f4a54a8e54d1cdb6b53d879b8d964b6666d
william <espiw@farmingdale.edu>
2026-02-28 11:58:52 -0500

    using user input with functions

--- functions.c
+++ functions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 // declare the parameters as (object type object, object type object)
 void happyBirthday(char name[], int age) {
@@ -13,8 +14,15 @@ int main() {
     // function = a reusable section of code that can be invoked "called"
     //            arguments can be sent to a function so that it can use them
 
-    char name[] = "william";
-    int age = 28;
+    char name[50] = "";
+    int age = 0;
+
+    printf("enter your name: ");
+    fgets(name, sizeof(name), stdin); // accept string input
+    name[strlen(name) - 1] = '\0'; // remove new line character from input buffer
+
+    printf("enter your age: ");
+    scanf("%d", &age);
 
     happyBirthday(name, age);
 
