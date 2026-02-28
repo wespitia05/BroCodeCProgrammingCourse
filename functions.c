86d7df06a0c14996700b5b085cb9126dfe981fd9
william <espiw@farmingdale.edu>
2026-02-28 11:39:25 -0500

    passing arguments using variables

--- functions.c
+++ functions.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-void happyBirthday() {
+// declare the parameters as (object type object, object type object)
+void happyBirthday(char name[], int age) {
     printf("\nhappy birthday to you!");
     printf("\nhappy birthday to you!");
-    printf("\nhappy birthday dear [name]!");
+    printf("\nhappy birthday dear %s!", name);
     printf("\nhappy birthday to you!");
-    printf("\nyou are [age] years old!");
+    printf("\nyou are %d years old!", age);
 }
 
 int main() {
     // function = a reusable section of code that can be invoked "called"
     //            arguments can be sent to a function so that it can use them
 
-    happyBirthday();
-    happyBirthday();
-    happyBirthday();
+    char name[] = "william";
+    int age = 28;
+
+    happyBirthday(name, age);
 
     return 0;
 }
