bed9ca66806ab6cb86ddc05ae66aec0ac904c043
william <espiw@farmingdale.edu>
2026-02-28 11:35:47 -0500

    calling function 3 times

--- functions.c
+++ functions.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+void happyBirthday() {
+    printf("\nhappy birthday to you!");
+    printf("\nhappy birthday to you!");
+    printf("\nhappy birthday dear [name]!");
+    printf("\nhappy birthday to you!");
+    printf("\nyou are [age] years old!");
+}
+
 int main() {
     // function = a reusable section of code that can be invoked "called"
     //            arguments can be sent to a function so that it can use them
+
+    happyBirthday();
+    happyBirthday();
+    happyBirthday();
 
     return 0;
 }
