25f8c0f9a6071442ede7f15e816f40e1f8e71e01
william <espiw@farmingdale.edu>
2026-02-28 16:49:57 -0500

    first few examples using int and double return statements

--- return_statements.c
+++ return_statements.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+// function data type is the same data type you are returning
+int square(int num) {
+    int result = num * num;
+
+    return result;
+}
+
+double squareDouble(double num) {
+    return num * num;
+}
+
+double cube(double num) {
+    return num * num * num;
+}
+
 int main() {
     // return = returns a value back to where you call the function
+
+    int x = square(2);
+    double y = squareDouble(3.4);
+    double z = cube(4.2);
+
+    printf("%d\n", x);
+    printf("%.2lf\n", y);
+    printf("%.2lf\n", z);
 
     return 0;
 }
