811fa14ae2930e8770a5e7a01229a87aefd040e9
william <espiw@farmingdale.edu>
2026-02-28 16:55:03 -0500

    variable scope setup

---
+++ variable_scope.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // variable scope = refers to where a variable is recognized and accessible
+    //                  variables can share the same name if
+    //                  they're in different scopes {}
+
+    return 0;
+}
