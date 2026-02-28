0562c45f0045c4832f7a55da3dcea7ae5969e096
william <espiw@farmingdale.edu>
2026-02-27 21:47:17 -0500

    functions setup

---
+++ functions.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // function = a reusable section of code that can be invoked "called"
+    //            arguments can be sent to a function so that it can use them
+
+    return 0;
+}
