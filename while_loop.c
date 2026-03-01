202f75c26d37f6a176f6c47e77a9388cd1a9b8ce
william <espiw@farmingdale.edu>
2026-03-01 13:15:11 -0500

    first example

--- while_loop.c
+++ while_loop.c
@@ -4,5 +4,12 @@ int main() {
     // while loop = continue some code WHILE the condition remains true
     //              condition must be true for us to enter while loop
 
+    int number = 0;
+
+    while(number <= 0) {
+        printf("enter a number greater than 0: ");
+        scanf("%d", &number);
+    }
+
     return 0;
 }
