29b5dbce96ad131c5025b5c97155e93813d40b93
william <espiw@farmingdale.edu>
2026-03-01 13:05:31 -0500

    function prototype setup

---
+++ function_prototype.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // function prototype = provide the compiler w/ information about a function's:
+    //                      name, return type, and parameters before its actual definition
+    //                      enables type checking and allows functions to be used before
+    //                      they're defined
+    //                      imporves readability, organization and helps prevent errors
+
+    return 0;
+}
