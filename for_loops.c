6c69cad3822923bc40f545a9945478b0dd8cfb83
william <espiw@farmingdale.edu>
2026-03-01 13:39:42 -0500

    counting example

--- for_loops.c
+++ for_loops.c
@@ -4,5 +4,15 @@ int main() {
     // for loop = repeat some code a limited # of times
     //            for(Initialization; Condition; Update)
 
+    // start at 1 and count to 10
+    for(int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    // start at 2 and count to 10 by twos
+    for(int i = 2; i <= 10; i+=2) {
+        printf("%d\n", i);
+    }
+
     return 0;
 }
