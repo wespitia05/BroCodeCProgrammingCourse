3e5b2133e449827fdd8284a2f0ba5fa53b7a059d
william <espiw@farmingdale.edu>
2026-03-01 13:12:27 -0500

    while loop setup

---
+++ while_loop.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // while loop = continue some code WHILE the condition remains true
+    //              condition must be true for us to enter while loop
+
+    return 0;
+}
