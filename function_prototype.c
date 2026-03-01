fcd95796631bd5f8435700aee0ad723eb2c62fa3
william <espiw@farmingdale.edu>
2026-03-01 13:11:12 -0500

    examples

--- function_prototype.c
+++ function_prototype.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+// function prototype, actual function declared after main function
+// without this, code won't work
+void hello(char name[], int age);
+bool ageCheck(int age);
 
 int main() {
     // function prototype = provide the compiler w/ information about a function's:
@@ -7,5 +13,28 @@ int main() {
     //                      they're defined
     //                      imporves readability, organization and helps prevent errors
 
+    hello("spongebob", 30);
+    if(ageCheck(30)) {
+        printf("you are old enough to work at the krusty krab");
+    }
+    else {
+        printf("you must be 16+ to work at the krusty krab");
+    }
+
     return 0;
+}
+
+// return type, function name (parameters)
+void hello(char name[], int age) {
+    printf("hello %s\n", name);
+    printf("you are %d years old\n", age);
+}
+
+bool ageCheck(int age) {
+    if (age >= 16) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
