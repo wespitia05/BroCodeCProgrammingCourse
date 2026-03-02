63a1a3b9252c094da4c3a0dec8b00e90a3d886d2
william <espiw@farmingdale.edu>
2026-03-02 18:29:13 -0500

    random number generator setup

---
+++ generate_random_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // pseudo-random = appear random but are determined by a 
+    //                 mathematical formula that uses a seed value
+    //                 to generate a predictable sequence of numbers.
+    //                 advanced: mersenne twister or /dev/random
+
+    return 0;
+}
