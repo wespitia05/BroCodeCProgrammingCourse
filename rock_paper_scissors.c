4d6a0d9320c41d818a5656b0e3bfc64ef18a5c80
william <espiw@farmingdale.edu>
2026-03-03 18:35:11 -0500

    declare our functions and variables

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -10,6 +10,25 @@ void checkWinner(int userChoice, int computerChoice);
 // ROCK PAPER SCISSORS //
 
 int main() {
+    // create seed value based on current time
+    srand(time(NULL));
+
+    printf("*** ROCK PAPER SCISSORS ***\n");
+
+    int userChoice = getUserChoice();
+    int computerChoice = getComputerChoice();
 
     return 0;
+}
+
+int getComputerChoice() {
+
+}
+
+int getUserChoice() {
+
+}
+
+void checkWinner(int userChoice, int computerChoice) {
+
 }
