4b0d653c84fa8f80e346031c146fe41ec20e42f2
william <espiw@farmingdale.edu>
2026-03-02 19:53:32 -0500

    functionality

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -17,5 +17,27 @@ int main() {
     // % will include 0 as a value, thats why we add 1
     int answer = (rand() % (max - min + 1)) + min;
 
+    printf("*** NUMBER GUESSING GAME ***\n");
+
+    // do this code while our guess doesn't equal our answer
+    do {
+        printf("guess a number between %d - %d: ", min, max);
+        scanf("%d", &guess);
+        tries++;
+
+        if(guess < answer) {
+            printf("TOO LOW!\n");
+        }
+        else if(guess > answer) {
+            printf("TOO HIGH!\n");
+        }
+        else {
+            printf("CORRECT!\n");
+        }
+    } while (guess != answer);
+
+    printf("the answer is %d\n", answer);
+    printf("it took you %d tries", tries);
+
     return 0;
 }
