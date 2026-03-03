0642d3aad2fdbfaef84967b5a16b9bb28aab2df3
william <espiw@farmingdale.edu>
2026-03-02 19:48:40 -0500

    declare our variables

--- number_guessing_game.c
+++ number_guessing_game.c
@@ -6,5 +6,16 @@
 
 int main() {
 
+    // create seed value based on current time
+    srand(time(NULL));
+
+    int guess = 0;
+    int tries = 0;
+    int min = 1;
+    int max = 100;
+    // formula for generating random number
+    // % will include 0 as a value, thats why we add 1
+    int answer = (rand() % (max - min + 1)) + min;
+
     return 0;
 }
