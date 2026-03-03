d8308d00c3fda74b6248156e947d698b99ca7cb3
william <espiw@farmingdale.edu>
2026-03-03 18:33:14 -0500

    declare our function prototypes

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+// function prototypes
+int getComputerChoice();
+int getUserChoice();
+void checkWinner(int userChoice, int computerChoice);
+
 // ROCK PAPER SCISSORS //
 
 int main() {
