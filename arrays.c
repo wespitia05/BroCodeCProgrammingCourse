d7d2bd8daec1f0ab5c34b99536770d6c8c014f00
william <espiw@farmingdale.edu>
2026-03-06 17:49:09 -0500

    printing integer values from an array

--- arrays.c
+++ arrays.c
@@ -4,5 +4,12 @@ int main() {
     // array = a fixed-size collection of elements of the same data type
     //         (similar to a variable, but it holds more than 1 value)
 
+    int numbers[] = {10, 20, 30, 40, 50};
+    printf("%d, ", numbers[0]);
+    printf("%d, ", numbers[1]);
+    printf("%d, ", numbers[2]);
+    printf("%d, ", numbers[3]);
+    printf("%d", numbers[4]);
+
     return 0;
 }
