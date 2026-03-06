85fb32d7d2388dd5900d448c7722ea836076c79f
william <espiw@farmingdale.edu>
2026-03-05 21:38:03 -0500

    arrays setup

---
+++ arrays.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // array = a fixed-size collection of elements of the same data type
+    //         (similar to a variable, but it holds more than 1 value)
+
+    return 0;
+}
