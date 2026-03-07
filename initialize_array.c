d762d433b85c94dce3486becacc1f1e20b9a16b0
william <espiw@farmingdale.edu>
2026-03-07 17:14:41 -0500

    example

--- initialize_array.c
+++ initialize_array.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    // set size of uninitialized array, {0} clears out any unusual behavior
+    int scores[5] = {0}; 
+
+    for(int i = 0; i < 5; i++) {
+        printf("enter a score: ");
+        scanf("%d", &scores[i]);
+    }
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", scores[i]);
+    }
 
     return 0;
 }
