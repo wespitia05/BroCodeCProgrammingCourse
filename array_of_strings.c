df110af8118a54bce61589ab8d4f10f4217b6485
william <espiw@farmingdale.edu>
2026-03-08 13:33:11 -0400

    printing out array of strings

--- array_of_strings.c
+++ array_of_strings.c
@@ -3,5 +3,11 @@
 int main() {
     // array of strings
 
+    char fruits[][10] = {"Apple", "Banana", "Coconut"};
+
+    for(int i = 0; i < 3; i++) {
+        printf("%s\n", fruits[i]);
+    }
+
     return 0;
 }
