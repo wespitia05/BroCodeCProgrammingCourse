bb0c992feede8f7ea8411d415f53aecdf3d68295
william <espiw@farmingdale.edu>
2026-03-08 13:20:08 -0400

    printing out number pad

--- 2d_array.c
+++ 2d_array.c
@@ -29,5 +29,16 @@ int main() {
         printf("\n");
     }
 
+    char numpad[][3] = {{'1', '2', '3'}, 
+                        {'4', '5', '6'}, 
+                        {'7', '8', '9'}, 
+                        {'*', '0', '#'}};
+    for(int i = 0; i < 4; i++) { // rows
+        for(int j = 0; j < 3; j++) { // columns
+            printf("%c ", numpad[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
