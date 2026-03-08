7a5ed4e680fe55ff96c6c7042d270b0ee28c5e25
william <espiw@farmingdale.edu>
2026-03-08 13:12:55 -0400

    printing out 2d array

--- 2d_array.c
+++ 2d_array.c
@@ -4,5 +4,21 @@ int main() {
     // 2D array = an array where each element is an array
     //            array[][] = {{}, {}, {}};
 
+    int numbers[][3] = {{1, 2, 3}, 
+                        {4, 5, 6}, 
+                        {7, 8, 9}};
+
+    printf("%d ", numbers[0][0]); // row 0, column 0
+    printf("%d ", numbers[0][1]); // row 0, column 1
+    printf("%d\n", numbers[0][2]); // row 0, column 2
+
+    printf("%d ", numbers[1][0]); // row 1, column 0
+    printf("%d ", numbers[1][1]); // row 1, column 1
+    printf("%d\n", numbers[1][2]); // row 1, column 2
+
+    printf("%d ", numbers[2][0]); // row 2, column 0
+    printf("%d ", numbers[2][1]); // row 2, column 1
+    printf("%d\n", numbers[2][2]); // row 2, column 2
+
     return 0;
 }
