b80e9021a2d0745fddc43540a487cce11d9058b0
william <espiw@farmingdale.edu>
2026-03-10 19:01:59 -0400

    finished our questions, options and answers

--- quiz_game.c
+++ quiz_game.c
@@ -6,12 +6,20 @@ int main() {
     // array of strings to hold all our questions (size of 100 characters)
     char questions[][100] = {
         "what is the largest planet in the solar system?",
-
+        "what is the hottest planet?",
+        "what planet has the most moons?",
+        "is the earth flat?"
     };
     // array of strings to hold all our answers (size of 100 characters)
     char options[][100] = {
         "A. jupiter\nB. saturn\nC. uranus\nD. neptune",
+        "A. mercury\nB. venus\nC. earth\nD. mars",
+        "A. earth\nB. mars\nC. jupiter\nD. saturn",
+        "A. yes\nB. no\nC.maybe\nD. sometimes"
     };
+
+    // 1D array of characters to hold our answers
+    char answerKey[] = {'A', 'B', 'D', 'B'};
 
     printf("%s\n", questions[0]);
     printf("%s", options[0]);
