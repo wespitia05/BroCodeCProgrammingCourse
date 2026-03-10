4f98417fc203c1aff4e6909050ed4432924eb722
william <espiw@farmingdale.edu>
2026-03-10 18:54:02 -0400

    create array of questions and options, test to check

--- quiz_game.c
+++ quiz_game.c
@@ -3,6 +3,18 @@
 // QUIZ GAME //
 
 int main() {
+    // array of strings to hold all our questions (size of 100 characters)
+    char questions[][100] = {
+        "what is the largest planet in the solar system?",
+
+    };
+    // array of strings to hold all our answers (size of 100 characters)
+    char options[][100] = {
+        "A. jupiter\nB. saturn\nC. uranus\nD. neptune",
+    };
+
+    printf("%s\n", questions[0]);
+    printf("%s", options[0]);
 
     return 0;
 }
