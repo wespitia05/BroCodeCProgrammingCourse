94e13b836c224f0bc5f97f8aec33890a1424385f
william <espiw@farmingdale.edu>
2026-03-12 19:25:21 -0400

    first example

--- ternary_operator.c
+++ ternary_operator.c
@@ -4,5 +4,12 @@ int main() {
     // ternary operator ? = shorthand for if-else statements
     // (condition) ? value_if_true : value_if_false;
 
+    int x = 7;
+    int y = 6;
+    // if x > y, return x otherwise return y
+    int max = (x > y) ? x : y;
+
+    printf("%d", max);
+
     return 0;
 }
