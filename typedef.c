2cabd2fb22d59bb779587ddd827cf9688778c29c
william <espiw@farmingdale.edu>
2026-03-13 14:43:27 -0400

    typedef setup

---
+++ typedef.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // typedef = reserved keyword that gives an existing datatype a "nickname"
+    //           helps simplify complex types and improves code readability
+
+    //           typedef existing_type new_name;
+
+    return 0;
+}
