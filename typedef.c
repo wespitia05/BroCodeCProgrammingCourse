4988e00d72ac6210a5d5754c4006d29a0e3a10dc
william <espiw@farmingdale.edu>
2026-03-13 15:44:07 -0400

    first example

--- typedef.c
+++ typedef.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
+// change int to Number
+typedef int Number;
+
 int main() {
     // typedef = reserved keyword that gives an existing datatype a "nickname"
     //           helps simplify complex types and improves code readability
 
     //           typedef existing_type new_name;
+
+    Number x = 3;
+    Number y = 4;
+    Number z = x + y;
+
+    printf("%d", z);
 
     return 0;
 }
