2eb25cd783533cdd2d7876dda04060a3b1d09ad7
william <espiw@farmingdale.edu>
2026-03-15 11:54:51 -0400

    enums setup

---
+++ enum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // enum = a user defined data type that consists
+    //        of a set of named integer constants
+    //        benefit: replaces numbers with readable names
+
+    // SUNDAY = 0;
+    // MONDAY = 1;
+    // TUESDAY = 2;
+
+    return 0;
+}
