ee6f313f4030173ee2dcf977d96c2d80953a6d0f
william <espiw@farmingdale.edu>
2026-03-15 12:07:28 -0400

    printing enum values

--- enum.c
+++ enum.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+// creating our enum constants
+typedef enum {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+} Day;
+
 int main() {
     // enum = a user defined data type that consists
     //        of a set of named integer constants
@@ -8,6 +13,12 @@ int main() {
     // SUNDAY = 0;
     // MONDAY = 1;
     // TUESDAY = 2;
+
+    Day today = SUNDAY;
+    Day tomorrow = MONDAY;
+
+    printf("%d", today); // will print value of sunday (0)
+    printf("%d", tomorrow); // will print value of monday (1)
 
     return 0;
 }
