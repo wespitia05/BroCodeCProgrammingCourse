3c6847e28a9a6d454093bf8d692383240a95c6c8
william <espiw@farmingdale.edu>
2026-03-15 12:13:47 -0400

    structs setup

---
+++ structs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // struct = a custom container that holds multiple
+    //          pieces of related information
+    //          similar to Objects in other languages
+
+    return 0;
+}
