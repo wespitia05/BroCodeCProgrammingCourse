99391ced57ac72fa2d2147173a0ecec79c3bb392
william <espiw@farmingdale.edu>
2026-03-16 19:35:48 -0400

    first example

--- structs.c
+++ structs.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+// very similar to object oriented programming (oop)
+typedef struct {
+    char name[50];
+    int age;
+    float gpa;
+    bool isFullTime;
+} Student;
 
 int main() {
     // struct = a custom container that holds multiple
     //          pieces of related information
     //          similar to Objects in other languages
+    Student student1 = {"Spongbob", 30, 2.5, true};
+
+    printf("%s\n", student1.name);
+    printf("%d\n", student1.age);
+    printf("%.2f\n", student1.gpa);
+    printf("%s\n", (student1.isFullTime) ? "yes" : "no");
 
     return 0;
 }
