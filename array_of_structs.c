413c17f874a41d3c44d754de60c2adc0ebdeb66d
william <espiw@farmingdale.edu>
2026-03-16 19:45:43 -0400

    array of structs setup

---
+++ array_of_structs.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    // array of structs = array where each element contains a struct {}
+    //                    helps organize and groups together related data
+
+    return 0;
+}
