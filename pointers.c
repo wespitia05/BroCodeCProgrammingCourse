899debfc5d580c06a50ba23998076af84efea4be
william <espiw@farmingdale.edu>
2026-03-17 19:55:01 -0400

    pointer example

--- pointers.c
+++ pointers.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void birthday(int* age);
+
 int main() {
     // pointer = a variable that stores the memory address of another variable
     //           benefit: they help avoid wasting memory by allowing you to pass
@@ -12,5 +14,15 @@ int main() {
     printf("%p\n", &age); // returns memory address of variable
     printf("%p\n", pAge); // returns same value of variable
 
+    birthday(pAge);
+
+    printf("you are %d years old", age);
+
     return 0;
+}
+
+// have to pass pointer integer
+void birthday(int* age) {
+    // pass by reference
+    (*age)++; // dereference age, then return value and increment
 }
