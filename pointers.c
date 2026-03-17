da0dc1e80da514129bdee1dac9dcc2c5863cc8f8
william <espiw@farmingdale.edu>
2026-03-17 19:39:55 -0400

    pointers setup

---
+++ pointers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // pointer = a variable that stores the memory address of another variable
+    //           benefit: they help avoid wasting memory by allowing you to pass
+    //           the address of a large data structure instead of copying the entire data
+
+    return 0;
+}
