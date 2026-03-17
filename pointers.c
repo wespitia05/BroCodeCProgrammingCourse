806bde147752aac959efff16c5e918b482872300
william <espiw@farmingdale.edu>
2026-03-17 19:45:25 -0400

    printing pointers

--- pointers.c
+++ pointers.c
@@ -5,5 +5,12 @@ int main() {
     //           benefit: they help avoid wasting memory by allowing you to pass
     //           the address of a large data structure instead of copying the entire data
 
+    int age = 25;
+    // * = dereference operator, & = gives you the address
+    int *pAge = &age;
+
+    printf("%p\n", &age); // returns memory address of variable
+    printf("%p\n", pAge); // returns same value of variable
+
     return 0;
 }
