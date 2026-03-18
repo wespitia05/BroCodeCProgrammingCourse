5a031a194846a2f6882c288b8d013de14178807c
william <espiw@farmingdale.edu>
2026-03-18 19:23:01 -0400

    opening the file

--- reading_files.c
+++ reading_files.c
@@ -3,5 +3,16 @@
 int main() {
     // READING FILES
 
+    // create pointer file struct, open it in the "read" mode
+    FILE *pFile = fopen("input.txt", "r");
+
+    // if pFile returns null it means it couldn't open/create the file
+    if(pFile == NULL) {
+        printf("could not open file\n");
+        return 1; // serves as an exit code
+    }
+
+    fclose(pFile); // always close your files once youre done
+
     return 0;
 }
