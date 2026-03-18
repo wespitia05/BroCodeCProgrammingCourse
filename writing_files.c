a61021d3a36a257455859f2844c716014c339c55
william <espiw@farmingdale.edu>
2026-03-17 20:09:44 -0400

    writing text into file

--- writing_files.c
+++ writing_files.c
@@ -6,11 +6,18 @@ int main() {
     // create pointer file struct, open it in the "write" mode
     FILE *pFile = fopen("output.txt", "w");
 
+    char text[] = "BOOTY BOOTY BOOTY\nROCKIN' EVERYWHERE!";
+
     // if pFile returns null it means it couldn't open/create the file
     if(pFile == NULL) {
         printf("error opening files\n");
         return 1; // serves as an exit code
     }
+
+    // file print, 3 parameters(pointer to file, format specifier, text)
+    fprintf(pFile, "%s", text);
+
+    printf("file was written successfully");
 
     fclose(pFile); // always close your files once youre done
 
