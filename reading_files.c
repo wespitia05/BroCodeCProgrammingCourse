1c37c518fe9846330592a0321058551dec76f4b1
william <espiw@farmingdale.edu>
2026-03-18 19:26:48 -0400

    reading the text file

--- reading_files.c
+++ reading_files.c
@@ -6,10 +6,17 @@ int main() {
     // create pointer file struct, open it in the "read" mode
     FILE *pFile = fopen("input.txt", "r");
 
+    char buffer[1024] = {0};
+
     // if pFile returns null it means it couldn't open/create the file
     if(pFile == NULL) {
         printf("could not open file\n");
         return 1; // serves as an exit code
+    }
+
+    // while the buffer being read isn't null, print out the contents
+    while(fgets(buffer, sizeof(buffer), pFile) != NULL) {
+        printf("%s", buffer);
     }
 
     fclose(pFile); // always close your files once youre done
