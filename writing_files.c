6f26ce1a08a6ef34e8478b24b8bcd3e5f346efe9
william <espiw@farmingdale.edu>
2026-03-17 20:05:09 -0400

    create a file we can write in

--- writing_files.c
+++ writing_files.c
@@ -3,5 +3,16 @@
 int main() {
     // WRITE A FILE 
 
+    // create pointer file struct, open it in the "write" mode
+    FILE *pFile = fopen("output.txt", "w");
+
+    // if pFile returns null it means it couldn't open/create the file
+    if(pFile == NULL) {
+        printf("error opening files\n");
+        return 1; // serves as an exit code
+    }
+
+    fclose(pFile); // always close your files once youre done
+
     return 0;
 }
