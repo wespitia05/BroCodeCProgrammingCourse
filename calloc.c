64835acaf462d422a0d1a049a2f26f88a195ba53
william <espiw@farmingdale.edu>
2026-03-19 19:32:02 -0400

    prompt user for number of players

--- calloc.c
+++ calloc.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     // calloc() = contigous allocation
     //            allocates memory dynamically and sets all allocated bytes to 0
     //            malloc() is faster, but calloc() leads to less bugs
     //            calloc(#, size)
+    
+    // prompt user for number of players
+    int number = 0;
+    printf("enter the number of players: ");
+    scanf("%d", &number);
 
     return 0;
 }
