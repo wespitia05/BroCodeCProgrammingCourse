15dcbc4ec6921d3c5a21e5e24253c9c39414ce74
william <espiw@farmingdale.edu>
2026-03-19 19:48:37 -0400

    prompt user for number of prices

--- realloc.c
+++ realloc.c
@@ -5,5 +5,10 @@ int main() {
     //             resize previously allocated memory
     //             realloc(ptr, bytes)
 
+    // prompt user for number of prices
+    int number = 0;
+    printf("enter the number of prices: ");
+    scanf("%d", &number);
+
     return 0;
 }
