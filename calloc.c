8b3435a1732a65b8b9a023ae638820563fa00cc3
william <espiw@farmingdale.edu>
2026-03-18 21:39:06 -0400

    calloc setup

---
+++ calloc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // calloc() = contigous allocation
+    //            allocates memory dynamically and sets all allocated bytes to 0
+    //            malloc() is faster, but calloc() leads to less bugs
+    //            calloc(#, size)
+
+    return 0;
+}
