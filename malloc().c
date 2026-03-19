07b8f06c5ea3bc9e13e1437e89d2f5f0a846fdc4
william <espiw@farmingdale.edu>
2026-03-18 21:30:37 -0400

    basic setup for memory allocation

--- malloc().c
+++ malloc().c
@@ -4,5 +4,17 @@ int main() {
     // malloc() = a function in C that dynamically allocates
     //            a specified number of bytes in memory
 
+    // prompt user to enter number of grades being inputted
+    int number = 0;
+    printf("enter the number of grades: ");
+    scanf("%d", &number);
+
+    // calculates how much space we need to reserve
+    // returns pointer to where the memory is located we reserve
+    char *grades = malloc(number * sizeof(char));
+
+    free(grades); // returning the "rented" space back to the OS
+    grades = NULL; // avoids dangling pointers, resets the pointer
+
     return 0;
 }
