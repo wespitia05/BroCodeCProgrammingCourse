99e2c3180b0f1fb8fb78869b3dee1411847a3778
william <espiw@farmingdale.edu>
2026-03-19 19:47:29 -0400

    realloc setup

---
+++ realloc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // realloc() = reallocation
+    //             resize previously allocated memory
+    //             realloc(ptr, bytes)
+
+    return 0;
+}
