418fa0bfaabd61ddf6c4735fad99130c9dc79203
william <espiw@farmingdale.edu>
2026-03-20 15:48:28 -0400

    declare constants and imports

--- digital_clock.c
+++ digital_clock.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 int main() {
     // DIGITAL CLOCK
+
+    time_t rawtime = 0; // rawtime holds the number of seconds
+    struct tm *pTime = NULL;
+    bool isRunning = true;
 
     return 0;
 }
