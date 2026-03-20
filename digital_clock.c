90132f0232fb70af0dcafadb6b300348e5a19b75
william <espiw@farmingdale.edu>
2026-03-20 16:07:55 -0400

    printing the time

--- digital_clock.c
+++ digital_clock.c
@@ -6,6 +6,7 @@
 int main() {
     // DIGITAL CLOCK
 
+    // reference point jan 1 1970 (Epoch)
     time_t rawtime = 0; // rawtime holds the number of seconds
     struct tm *pTime = NULL;
     bool isRunning = true;
@@ -13,7 +14,17 @@ int main() {
     printf("DIGITAL CLOCK\n");
 
     while(isRunning) {
-        printf("test\n");
+        time(&rawtime); // update the time with the current time
+
+        // displays amount of time that has passed since reference point
+        // printf("%ld\n", rawtime);
+
+        // returns pointer to a time struct with that contains fields of hours, minutes, seconds
+        pTime = localtime(&rawtime); 
+
+        // print the time, \r makes the line update rather than printing and entire new line
+        printf("\r%02d:%02d:%02d", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
+        
         sleep(1);
     }
 
