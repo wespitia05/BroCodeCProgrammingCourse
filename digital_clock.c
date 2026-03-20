78d41c5aa966950a86f7bfb9b2b196f1d150adda
william <espiw@farmingdale.edu>
2026-03-20 15:50:40 -0400

    testing to make sure sleep function is working properly

--- digital_clock.c
+++ digital_clock.c
@@ -10,5 +10,12 @@ int main() {
     struct tm *pTime = NULL;
     bool isRunning = true;
 
+    printf("DIGITAL CLOCK\n");
+
+    while(isRunning) {
+        printf("test\n");
+        sleep(1);
+    }
+
     return 0;
 }
